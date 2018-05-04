c8a6bf4a929ffbd8698bac9212fbe952aefa7a6f
Chris Mousdale <chris@mousdale.com>
2018-05-04 10:57:39 +0100

    Tweaks

--- js/script.js
+++ js/script.js
@@ -25,11 +25,11 @@ var vowel = /\b(a)\b(\s+)?(((<[^>]+>)\s?)+)?(\s+)?([aeiou]|hou)/gim;
 function initialise() {
 
 	dom.generate.click(function(){
-	
+
 		update();
 		return false;
 	});
-	
+
 	regex = generateRegExp();
 	update();
 }
@@ -41,7 +41,7 @@ function update() {
 	dom.output.html(generateIdea());
 	dom.output.hide();
 	dom.output.fadeIn(500);
-	
+
 	setGenerateLabel();
 }
 
@@ -52,55 +52,55 @@ function generateRegExp() {
 	var str = '';
 	var arr = [];
 	var tmp = "@(types)";
-	
+
 	for(type in corpus) {
 		arr.push(type);
 	}
-	
+
 	var exp = tmp.replace("types", arr.join('|'));
-	
+
 	return new RegExp(exp, "ig");
 }
 
 // generate idea
 
 function generateIdea() {
-	
+
 	var type;
 	var match;
 	var index;
 	var intro;
 	var output;
-	
+
 	var template = templates[(Math.random() * templates.length) | 0];
-	
+
 	var data = {};
-	
+
 	for(var prop in corpus) {
 		data[prop] = corpus[prop].concat();
 	}
-	
+
 	var result = regex.exec(template);
-	
+
 	while(result) {
-	
+
 		type = result[1];
 		match = result[0];
-		
+
 		index = (Math.random() * data[type].length) | 0;
 		template = template.replace(match, data[type].splice(index, 1)[0]);
-		
+
 		regex.lastIndex = 0;
 		result = regex.exec(template);
 	}
-	
+
 	var intro = phrases[(Math.random() * phrases.length) | 0];
-	
+
 	output = "<dl>";
 	output += "<dt>" + intro + "</dt>";
 	output += "<dd>" + template + "</dd>";
 	output += "</dl>";
-	
+
 	return correctGrammar(output);
 }
 
@@ -119,8 +119,8 @@ function correctGrammar(input) {
 function setGenerateLabel() {
 
 	var label = labels[(Math.random() * labels.length) | 0];
-	dom.generate.text(label);
-	
+	dom.generate.text(label + '- Next Trick!');
+
 }
 
 // ready
@@ -129,11 +129,11 @@ $(document).ready(function(){
 
 	dom.output = $("#output");
 	dom.generate = $("#generate");
-	
+
 	if(corpus) {
 		initialise();
 	} else {
 		//console.log("corpus not found");
 	}
-	
+
 });
